dbdf1e3a8587d4feec568a1a1781699b7b2c0cff
Naveen <chnaveen191@gmail.com>
2021-07-18 09:40:09 +0530

    updatedMLops

---
+++ main.py
@@ -0,0 +1,73 @@
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel
+from ml_utils import load_model, predict, retrain
+from typing import List
+from datetime import datetime, time
+
+# defining the main app
+app = FastAPI(title="GermanCreditScoring", docs_url="/")
+
+# calling the load_model during startup.
+# this will train the model and keep it loaded for prediction.
+app.add_event_handler("startup", load_model)
+
+# class which is expected in the payload
+class QueryIn(BaseModel):
+    duration_in_month: int
+    credit_history: str
+    purpose: str
+    credit_amount: float
+    present_employment_since_in_yrs: float
+    installment_rate_in_percent_of_income: float
+    job: str
+    housing: str
+    no_of_people_liable_to_provide_maintainence_for: int
+    foreign_worker: str
+# class which is returned in the response
+class QueryOut(BaseModel):
+    loan_status: int
+    time_stamp: time
+
+# class which is expected in the payload while re-training
+class FeedbackIn(BaseModel):
+    duration_in_month: int
+    credit_history: str
+    purpose: str
+    credit_amount: float
+    present_employment_since_in_yrs: float
+    installment_rate_in_percent_of_income: float
+    job: str
+    housing: str
+    no_of_people_liable_to_provide_maintainence_for: int
+    foreign_worker: str
+    loan_status: int
+# Route definitions
+@app.get("/ping")
+# Healthcheck route to ensure that the API is up and running
+def ping():
+    return {"ping": "pong"}
+
+
+@app.post("/predict_", response_model=QueryOut, status_code=200)
+# Route to do the prediction using the ML model defined.
+# Payload: QueryIn containing the parameters
+# Response: QueryOut containing the flower_class predicted (200)
+def predict_loan_status(query_data: QueryIn):
+    return {"loan_status": predict(query_data),
+            "time_stamp": datetime.now().time()}
+
+@app.post("/feedback_loop", status_code=200)
+# Route to further train the model based on user input in form of feedback loop
+# Payload: FeedbackIn containing the parameters and correct flower class
+# Response: Dict with detail confirming success (200)
+def feedback_loop(data: List[FeedbackIn]):
+    retrain(data)
+    return {"detail": "Feedback loop successful",
+            "time_stamp": datetime.now().time()}
+
+
+# Main function to start the app when main.py is called
+if __name__ == "__main__":
+    # Uvicorn is used to run the server and listen for incoming API requests on 0.0.0.0:8888
+    uvicorn.run("main:app", host="0.0.0.0", port=8888, reload=True)
---
+++ mlutils.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import numpy as np
+import copy
+from collections import Counter
+from matplotlib import pyplot as plt
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.model_selection import cross_val_score
+import seaborn as sns
+from sklearn.metrics import fbeta_score, f1_score,precision_score,recall_score,accuracy_score, roc_curve, auc,confusion_matrix
+from sklearn.compose import ColumnTransformer
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_predict
+from sklearn.metrics import roc_curve
+from sklearn.metrics import roc_auc_score
+from credit_actual_data_values import substitute
+
+# define a GradientBoostingclassifier
+clf = GradientBoostingClassifier()
+# define a SVM classifier
+classes = {1: "GoodRisk", 2: "BadRisk"}
+r_classes = {y: x for x, y in classes.items()}
+
+# function to train and load the model during startup
+def load_model():
+    # load the dataset from the official sklearn datasets
+    df=pd.read_csv('germanCreditData_hackathon.data', sep=" ", header=None)    # do the test-train split and train the model
+    last_ix = len(df.columns) - 1
+    X, y = df.drop(last_ix, axis=1), df[last_ix]
+    # Categorical features has to be converted into integer values for the model to process. 
+    #This is done through one hot encoding.
+    # select categorical features
+    cat_ix = X.select_dtypes(include=['object', 'bool']).columns
+    # one hot encode categorical features only
+    ct = ColumnTransformer([('o',OneHotEncoder(),cat_ix)], remainder='passthrough')
+    X = ct.fit_transform(X)
+    # label encode the target variable to have the classes 0 and 1
+    y = LabelEncoder().fit_transform(y)
+    print(X.shape, y.shape, Counter(y))
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    clf.fit(X_train, y_train)
+    # calculate and print the accuracy score for GaussianNB
+    clf_acc = accuracy_score(y_test, clf.predict(X_test))
+    print(f"Model GradientBoost trained with accuracy: {round(clf_acc, 3)}")
+    clf_precision = precision_score(y_test,y_pred, average='micro')
+    print(f"Model GradientBoost trained with precision: {round(clf_precision, 3)}")
+    clf_recall = recall_score(y_test,y_pred, average='micro')
+    print(f"Model GradientBoost trained with recall: {round(clf_recall, 3)}")
+    clf_f1 = f1_score(y_test,y_pred, average='macro')
+    print(f"Model GradientBoost trained with f1score: {round(clf_f1, 3)}")
+   
+# function to predict the flower using the model
+def predict(query_data):
+    x = list(query_data.dict().values())
+    prediction = clf.predict([x])[0]
+    print(f"Model prediction: {classes[prediction]}")
+    return classes[prediction]
+
+# function to retrain the model as part of the feedback loop
+def retrain(data):
+    # pull out the relevant X and y from the FeedbackIn object
+    X = [list(d.dict().values())[:-1] for d in data]
+    y = [r_classes[d.flower_class] for d in data]
+
+    # fit the classifier again based on the new data obtained
+    clf.fit(X, y)
